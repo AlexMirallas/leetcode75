d75c8229a94154dbcedd9cf9c8e63a6ebd9a3cc2
AlexMirallas <command.shockwave1@gmail.com>
2024-04-12 19:07:08 +0200

    first problem solved :D

--- two_sum.py
+++ two_sum.py
@@ -1,12 +1,16 @@
-def two_sum(nums,target):
-    idx =[]
-    for i in range(len(nums)):
-        counter =1
-        if nums[i] + nums[i+counter] == target:
-            idx.append(i,i+counter)
-            break
-        else:
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
             
             
-            
-    
+nums = [3,2,3]
+target = 6
+
+print(two_sum(nums,target))
+
+
